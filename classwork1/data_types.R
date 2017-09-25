b95c0c3bed490a7d89d7138775958b463bc3954c
Sergey Mirvoda <mirvoda_sg@octonica.corp>
2017-09-25 13:03:19 +0500

    Введение в R. Структуры данных. Первые лабы

---
+++ classwork1/data_types.R
@@ -0,0 +1,17 @@
+# Присвойте значение 33.3 переменной my_numeric
+my_numeric <- 
+# Проверьте тип данных переменной
+class(my_numeric)
+
+# Смените значение my_numeric на 42
+
+
+# Проверьте тип данных my_numeric
+
+# Установите значение my_character в "МИР"
+my_character <- "Привет"
+# Проверьте тип данных my_numeric
+
+# Установите my_logical в FALSE
+my_logical <- TRUE
+# Проверьте тип данных my_numeric
---
+++ classwork1/data_types_conversion.R
@@ -0,0 +1,13 @@
+# Проверка и преобразование типов is.XYS as.XYZ 
+
+# Присвойте значение 33.3 переменной my_numeric
+my_numeric <- 
+
+# Установите значение my_character в "33"
+my_character <- "Привет"
+# Проверьте тип данных my_character
+is.numeric(my_character)
+# Преобразуйте my_character в число при помощи функции as.numeric и запишите в переменную my_numeric_character
+
+my_numeric_character<-
+# Пребразуйте сумму my_numeric и my_numeric_character и выведите на экран
