7672711d53b88af8302c2a4b0c3d9fe31d631ac9
Zubareva Marina <32264363+zubarevnka@users.noreply.github.com>
2017-11-30 14:59:44 +0500

    Add files via upload

---
+++ Zubareva/Classwork/intro1_classwork.R
@@ -0,0 +1,21 @@
+#uno
+35 + 777
+13 * 47
+675 * 15
+849 * 629
+#dos
+11111111 * 11111111
+#11111111 * 1111111 ?
+#tres
+a <- c(3, 7, 12, 15, 20)
+b <- c(2, 5, 8, 11, 15)
+a * b
+#cuatro
+plot(a, b, type = "p", xlab = "a", ylab = "b")
+lines(a, b)
+#cinco
+time <- c("Math" = 40, "English" = 40, "PE" = 10, "Programming" = 150)
+barplot(time, main = "Timetable")
+#seis
+drinks <- rnorm(10000, mean = 450, sd = 4)
+mean(drinks > 455)
