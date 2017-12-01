9a291d4f23c23b70207f334e9041aa949e4ac9be
Zubareva Marina <32264363+zubarevnka@users.noreply.github.com>
2017-12-01 18:09:08 +0500

    Update regex.R

--- Zubareva/Classwork/CW_3/regex.R
+++ Zubareva/Classwork/CW_3/regex.R
@@ -1,8 +1,10 @@
-###нужно сделать
-
 #Загрузите данные о землятресениях
 anss <- readLines("https://raw.githubusercontent.com/SergeyMirvoda/MD-DA-2017/master/data/earthquakes_2011.html", warn=FALSE)
 
 #Выберите строки, которые содержат данные с помощью регулярных выражений и функции grep
+expstr <- "\\d{4}\\/\\d{2}\\/\\d{2} \\d{2}:\\d{2}:\\d{2}.\\d{2},\\W?\\d*.\\d{4},\\W?\\d*.\\d{4},\\d+.\\d{2},\\d+.\\d{2},\\w*,\\d+,\\d*,\\d*,\\d*.\\d{2},\\w+,\\d*"
+data <- grep(expstr, anss)
 
-#Проверьте, что все строки (all.equal) в результирующем векторе подходят под шаблон. 
+#Проверьте, что все строки (all.equal) в результирующем векторе подходят под шаблон.
+pre <- grep("PRE", anss)
+all.equal(length(data), pre[2] - pre[1] - 2)
